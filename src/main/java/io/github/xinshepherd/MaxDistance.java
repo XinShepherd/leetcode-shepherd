da512a29dff20529074304461fd883eb5189cab2
Fuxin <okfuxin@gmail.com>
2020-03-29 09:31:07 +0800

    1162. 地图分析

---
+++ src/main/java/io/github/xinshepherd/MaxDistance.java
@@ -0,0 +1,74 @@
+package io.github.xinshepherd;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 1162. 地图分析
+ *
+ * @author Fuxin
+ * @since 2020/3/29
+ */
+public class MaxDistance {
+
+    int[] nr = {0, 1, 0, -1};
+    int[] nc = {-1, 0, 1, 0};
+
+    public int maxDistance(int[][] grid) {
+        int n = grid.length;
+        if (n == 0) return 0;
+        int[][] dp = new int[n][n];
+
+        Deque<Integer> queue = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int v = grid[i][j];
+                if (v == 1) {
+                    dp[i][j] = 0;
+                    int code = i * n + j;
+                    queue.addLast(code);
+                }
+            }
+        }
+        int max = -1;
+        while (!queue.isEmpty()) {
+            Integer code = queue.poll();
+            int dr = code / n;
+            int dc = code % n;
+            for (int i = 0; i < 4; i++) {
+                int r = dr + nr[i];
+                int c = dc + nc[i];
+                if (r >= 0 && r < n && c >= 0 && c < n && grid[r][c] == 0) {
+                    dp[r][c] = dp[dr][dc] + 1;
+                    grid[r][c] = 1;
+                    int newCode = r * n + c;
+                    queue.addLast(newCode);
+                    max = Math.max(max, dp[r][c]);
+                }
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        MaxDistance distance = new MaxDistance();
+        System.out.println(distance.maxDistance(new int[][]{
+                new int[]{1, 0, 1},
+                new int[]{0, 0, 0},
+                new int[]{1, 0, 1}
+        }));
+
+        System.out.println(distance.maxDistance(new int[][]{
+                new int[]{1, 0, 0},
+                new int[]{0, 0, 0},
+                new int[]{0, 0, 0}
+        }));
+        System.out.println(distance.maxDistance(new int[][]{
+                new int[]{0, 0, 0},
+                new int[]{0, 0, 0},
+                new int[]{0, 0, 0}
+        }));
+
+    }
+}
