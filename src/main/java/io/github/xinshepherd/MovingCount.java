5f8beb6e83b83e19b5eb1b7b880c1611f31d2251
Fuxin <okfuxin@gmail.com>
2020-04-08 16:20:25 +0800

    面试题13. 机器人的运动范围 （待优化）

---
+++ src/main/java/io/github/xinshepherd/MovingCount.java
@@ -0,0 +1,61 @@
+package io.github.xinshepherd;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 面试题13. 机器人的运动范围
+ *
+ * @author Fuxin
+ * @since 2020/4/8
+ */
+public class MovingCount {
+
+    int[] nr = {0, -1, 0, 1};
+    int[] nc = {-1, 0, 1, 0};
+
+    public int movingCount(int m, int n, int k) {
+        Deque<Integer> stack = new LinkedList<>();
+        boolean[][] dp = new boolean[m][n];
+        dp[0][0] = true;
+        stack.push(0);
+        int divisor = Math.max(m, n);
+        int count = 1;
+        while (!stack.isEmpty()) {
+            int code = stack.pop();
+            int dr = code / divisor;
+            int dc = code % divisor;
+            for (int i = 0; i < 4; i++) {
+                int x = dr + nr[i];
+                int y = dc + nc[i];
+                if (x >= 0 && x < m && y >= 0 && y < n && !dp[x][y]) {
+                    int sum = sumDigits(x) + sumDigits(y);
+                    if (sum <= k) {
+                        dp[x][y] = true;
+                        int c = x * divisor + y;
+                        stack.push(c);
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+
+    int sumDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num /= 10;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        MovingCount count = new MovingCount();
+        System.out.println(count.movingCount(2, 3, 1));
+        System.out.println(count.movingCount(3, 1, 0));
+        System.out.println(count.movingCount(1, 2, 1));
+    }
+
+}
