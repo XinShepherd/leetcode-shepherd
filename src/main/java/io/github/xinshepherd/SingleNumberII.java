c3639cb3f1338cfad2d940684f14979c0df2f180
Fuxin <okfuxin@gmail.com>
2020-04-27 09:12:59 +0800

    137. 只出现一次的数字 II

---
+++ src/main/java/io/github/xinshepherd/SingleNumberII.java
@@ -0,0 +1,41 @@
+package io.github.xinshepherd;
+
+/**
+ * 137. 只出现一次的数字 II
+ *
+ * @author Fuxin
+ * @since 2020/4/27
+ */
+public class SingleNumberII {
+
+    public int singleNumber(int[] nums) {
+        int seenOnce = 0, seenTwice = 0;
+
+        // 交换律
+        for (int num : nums) {
+            // first appearence:
+            // add num to seen_once
+            // don't add to seen_twice because of presence in seen_once
+
+            // second appearance:
+            // remove num from seen_once
+            // add num to seen_twice
+
+            // third appearance:
+            // don't add to seen_once because of presence in seen_twice
+            // remove num from seen_twice
+            seenOnce = ~seenTwice & (seenOnce ^ num);
+            seenTwice = ~seenOnce & (seenTwice ^ num);
+        }
+
+        return seenOnce;
+
+    }
+
+    public static void main(String[] args) {
+        SingleNumberII singleNumberII = new SingleNumberII();
+        System.out.println(singleNumberII.singleNumber(new int[]{2, 3, 2, 2}));
+
+    }
+
+}
