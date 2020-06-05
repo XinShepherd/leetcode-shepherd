b6671116975dc727332c81fc34486057f383d0dd
Fuxin <okfuxin@gmail.com>
2020-06-05 08:56:21 +0800

    238. 除自身以外数组的乘积

--- src/main/java/io/github/xinshepherd/ProductExceptSelf.java
+++ src/main/java/io/github/xinshepherd/ProductExceptSelf.java
@@ -1,5 +1,7 @@
 package io.github.xinshepherd;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -11,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class ProductExceptSelf {
 
+    // O(n) 空间复杂度 O(n) 时间复杂度
     public int[] productExceptSelf(int[] nums) {
         if (nums.length == 0)
             return nums;
@@ -31,11 +34,31 @@ public class ProductExceptSelf {
         return ans;
     }
 
+    // O(1) 空间复杂度 O(n) 时间复杂度
+    public int[] productExceptSelf2(int[] nums) {
+        if (nums.length == 0)
+            return nums;
+        int len = nums.length;
+        int[] ans = new int[len];
+        Arrays.fill(ans, 1);
+        int product = 1;
+        for (int i = 0; i < len; i++) {
+            ans[i] = product;
+            product *= nums[i];
+        }
+        product = 1;
+        for (int i = len - 1; i >= 0; i--) {
+            ans[i] *= product;
+            product *= nums[i];
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         ProductExceptSelf self = new ProductExceptSelf();
-        assertThat(self.productExceptSelf(new int[]{1, 2, 3, 4})).isEqualTo(new int[]{24, 12, 8, 6});
-        assertThat(self.productExceptSelf(new int[]{1, 0, 3, 4})).isEqualTo(new int[]{0, 12, 0, 0});
-        assertThat(self.productExceptSelf(new int[]{1, -1, 3, 4})).isEqualTo(new int[]{-12, 12, -4, -3});
+        assertThat(self.productExceptSelf2(new int[]{1, 2, 3, 4})).isEqualTo(new int[]{24, 12, 8, 6});
+        assertThat(self.productExceptSelf2(new int[]{1, 0, 3, 4})).isEqualTo(new int[]{0, 12, 0, 0});
+        assertThat(self.productExceptSelf2(new int[]{1, -1, 3, 4})).isEqualTo(new int[]{-12, 12, -4, -3});
     }
 
 }
