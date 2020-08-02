381431da2ee292f8a205afeb4bcb9e276857f5cb
Fuxin <okfuxin@gmail.com>
2020-08-02 11:24:01 +0800

    114. 二叉树展开为链表

--- src/main/java/io/github/xinshepherd/Flatten.java
+++ src/main/java/io/github/xinshepherd/Flatten.java
@@ -1,5 +1,7 @@
 package io.github.xinshepherd;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * 114. 二叉树展开为链表
  * https://leetcode-cn.com/problems/flatten-binary-tree-to-linked-list/
@@ -27,11 +29,32 @@ public class Flatten {
         return flatten(flattened, right);
     }
 
+    public void flatten2(TreeNode root) {
+        if (root == null) return;
+        flat(root, root.left, root.right);
+    }
+
+    private TreeNode flat(TreeNode node, TreeNode left, TreeNode right) {
+        if (left != null) {
+            node.left = null;
+            node.right = left;
+            node = flat(left, left.left, left.right);
+        }
+        if (right != null) {
+            node.right = right;
+            node = flat(right, right.left, right.right);
+        }
+        return node;
+    }
+
     public static void main(String[] args) {
         Flatten flatten = new Flatten();
         TreeNode demo = TreeNode.demo2();
         flatten.flatten(demo);
         System.out.println(demo);
+        TreeNode demo2 = TreeNode.demo2();
+        flatten.flatten2(demo2);
+        assertThat(demo2.val).isEqualTo(3);
     }
 
 }
