1228cdd1ad3bb1345207c90fd340b5d4a4060f54
Fuxin <okfuxin@gmail.com>
2020-08-19 20:11:04 +0800

    647. 回文子串 中心扩展法

--- src/main/java/io/github/xinshepherd/CountSubstrings.java
+++ src/main/java/io/github/xinshepherd/CountSubstrings.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 public class CountSubstrings {
 
+    // 暴力破解
     public int countSubstrings(String s) {
         char[] chars = s.toCharArray();
         int count = 0;
@@ -32,11 +33,32 @@ public class CountSubstrings {
         return true;
     }
 
+    // 中心扩展法
+    public int countSubstrings2(String s) {
+        char[] chars = s.toCharArray();
+        int n = chars.length;
+        int count = 0;
+        for (int i = 0; i < n * 2 - 1 ; i++) {
+            int l = i / 2;
+            int r = i / 2 + i % 2;
+            while (l >= 0 && r < n && chars[l] == chars[r]) {
+                l--;
+                r++;
+                count++;
+            }
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
         CountSubstrings countSubstrings = new CountSubstrings();
         assertThat(countSubstrings.countSubstrings("")).isEqualTo(0);
         assertThat(countSubstrings.countSubstrings("abc")).isEqualTo(3);
         assertThat(countSubstrings.countSubstrings("aaa")).isEqualTo(6);
+        assertThat(countSubstrings.countSubstrings2("")).isEqualTo(0);
+        assertThat(countSubstrings.countSubstrings2("abc")).isEqualTo(3);
+        assertThat(countSubstrings.countSubstrings2("aaa")).isEqualTo(6);
     }
 
 }
