fc9037b17ee8dfe943b53c611cd4a3a6f1abf2a8
fuxin <okfuxin@gmail>
2020-10-31 13:26:11 +0800

    381. O(1) 时间插入、删除和获取随机元素 - 允许重复

--- src/main/java/io/github/xinshepherd/RandomizedCollection.java
+++ src/main/java/io/github/xinshepherd/RandomizedCollection.java
@@ -1,6 +1,10 @@
 package io.github.xinshepherd;
 
-import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * @author Fuxin
@@ -8,7 +12,25 @@ import java.util.LinkedHashSet;
  */
 public class RandomizedCollection {
 
-    private LinkedHashSet<Integer> container = new LinkedHashSet<>();
+    static class Node {
+        int val;
+        Node before;
+        Node after;
+        Node next;
+
+        public Node(int val) {
+            this.val = val;
+        }
+    }
+
+    Map<Integer, Node> map = new HashMap<>();
+    Node head;
+    Node tail;
+    Node current;
+
+    int size = 0;
+    boolean changed = false;
+    int[] array;
 
     /**
      * Initialize your data structure here.
@@ -21,21 +43,79 @@ public class RandomizedCollection {
      * Inserts a value to the collection. Returns true if the collection did not already contain the specified element.
      */
     public boolean insert(int val) {
-        return container.add(val);
+        Node node = new Node(val);
+        if (head == null) {
+            head = node;
+        } else {
+            tail.after = node;
+            node.before = tail;
+        }
+        tail = node;
+        Node valNode = map.get(val);
+        boolean notContain = valNode == null;
+        if (!notContain) {
+            node.next = valNode;
+        }
+        map.put(val, node);
+        current = head;
+        changed = true;
+        size++;
+        return notContain;
     }
 
     /**
      * Removes a value from the collection. Returns true if the collection contained the specified element.
      */
     public boolean remove(int val) {
-        return container.remove(val);
+        Node node = map.get(val);
+        boolean contains = node != null;
+        if (contains) {
+            map.put(val, node.next);
+            Node before = node.before;
+            if (before != null) {
+                before.after = node.after;
+            }
+            if (node.after != null) {
+                node.after.before = before;
+            }
+            if (node == head) {
+                head = node.after;
+            }
+            if (node == tail) {
+                tail = node.before;
+            }
+            current = head;
+            changed = true;
+            size--;
+        }
+        return contains;
     }
 
     /**
      * Get a random element from the collection.
      */
     public int getRandom() {
+        if (changed) {
+            array = new int[size];
+            Node cur = head;
+            for (int i = 0; i < size; i++) {
+                array[i] = cur.val;
+                cur = cur.after;
+            }
+        }
+        return array[ThreadLocalRandom.current().nextInt(size)];
+    }
 
+    public int getRandom2() {
+        if (current == null) {
+            current = head;
+        }
+        if (current != null) {
+            int val = current.val;
+            current = current.after;
+            return val;
+        }
+        return 0;
     }
 
     /**
@@ -49,22 +129,17 @@ public class RandomizedCollection {
         // 初始化一个空的集合。
         RandomizedCollection collection = new RandomizedCollection();
 
-        // 向集合中插入 1 。返回 true 表示集合不包含 1 。
-        collection.insert(1);
+        assertThat(collection.insert(1)).isTrue();
 
-        // 向集合中插入另一个 1 。返回 false 表示集合包含 1 。集合现在包含 [1,1] 。
-        collection.insert(1);
+        assertThat(collection.insert(10)).isTrue();
 
-        // 向集合中插入 2 ，返回 true 。集合现在包含 [1,1,2] 。
-        collection.insert(2);
+        assertThat(collection.insert(10)).isFalse();
 
-        // getRandom 应当有 2/3 的概率返回 1 ，1/3 的概率返回 2 。
-        collection.getRandom();
+        assertThat(collection.insert(100)).isTrue();
 
-        // 从集合中删除 1 ，返回 true 。集合现在包含 [1,2] 。
-        collection.remove(1);
+        assertThat(collection.getRandom());
 
-        // getRandom 应有相同概率返回 1 和 2 。
-        collection.getRandom();
+        assertThat(collection.remove(10)).isTrue();
     }
+
 }
