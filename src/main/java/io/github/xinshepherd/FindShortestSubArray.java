52dd0795e5272505031bb94635f90b11e377acdc
Fuxin <okfuxin@gmail.com>
2021-02-20 13:57:33 +0800

    697. 数组的度

--- src/main/java/io/github/xinshepherd/FindShortestSubArray.java
+++ src/main/java/io/github/xinshepherd/FindShortestSubArray.java
@@ -1,13 +1,10 @@
 package io.github.xinshepherd;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * 697. 数组的度
- *
+ * <p>
  * https://leetcode-cn.com/problems/degree-of-an-array/
  *
  * @author Fuxin
@@ -17,18 +14,17 @@ public class FindShortestSubArray {
 
     public int findShortestSubArray(int[] nums) {
         int length = nums.length;
-        Map<Integer, Number> map = new HashMap<>(length);
+        Number[] data = new Number[50000];
         int maxCount = 0;
         for (int i = 0; i < length; i++) {
             int num = nums[i];
-            Number number = map.get(num);
+            Number number = data[num];
             if (number == null) {
                 number = new Number();
-                number.num = num;
                 number.count = 1;
                 number.start = i;
                 number.end = i;
-                map.put(num, number);
+                data[num] = number;
             } else {
                 number.count++;
                 number.end = i;
@@ -38,7 +34,8 @@ public class FindShortestSubArray {
             }
         }
         int minSub = Integer.MAX_VALUE;
-        for (Number value : map.values()) {
+        for (int num : nums) {
+            Number value = data[num];
             if (value.count == maxCount) {
                 minSub = Math.min(minSub, value.end - value.start + 1);
             }
@@ -47,16 +44,46 @@ public class FindShortestSubArray {
     }
 
     static class Number {
-        int num;
         int count;
         int start;
         int end;
+    }
+
+    public int findShortestSubArray2(int[] nums) {
+        int max = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
+        for (int i : nums) {
+            max = Math.max(i, max);
+            min = Math.min(i, min);
+        }
+        int[] contnums = new int[max - min + 1];
+        int degree = 0;
+        for (int i : nums) {
+            degree = Math.max(degree, ++contnums[i - min]);
+        }
+        if (degree == 1) return 1;
+        int result = nums.length;
+        for (int i = 0; i < contnums.length; i++) {
+            if (contnums[i] == degree) {
+                int tmp = min + i;
+                int start = 0, end = nums.length - 1;
+                while (start < end && nums[start] != tmp) {
+                    start++;
+                }
+                while (start < end && nums[end] != tmp) {
+                    end--;
+                }
+                result = Math.min(result, end - start + 1);
+            }
+        }
+        return result;
     }
 
     public static void main(String[] args) {
         FindShortestSubArray findShortestSubArray = new FindShortestSubArray();
         assertThat(findShortestSubArray.findShortestSubArray(new int[]{1, 2, 2, 3, 1})).isEqualTo(2);
         assertThat(findShortestSubArray.findShortestSubArray(new int[]{1, 2, 2, 3, 1, 4, 2})).isEqualTo(6);
+        assertThat(findShortestSubArray.findShortestSubArray2(new int[]{1, 2, 2, 3, 1})).isEqualTo(2);
+        assertThat(findShortestSubArray.findShortestSubArray2(new int[]{1, 2, 2, 3, 1, 4, 2})).isEqualTo(6);
     }
 
 }
