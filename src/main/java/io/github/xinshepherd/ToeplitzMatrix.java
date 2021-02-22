ea0a8f5b7807d85bb101aa5f6d649779919aef4b
Fuxin <okfuxin@gmail.com>
2021-02-22 08:45:40 +0800

    766. 托普利茨矩阵

---
+++ src/main/java/io/github/xinshepherd/ToeplitzMatrix.java
@@ -0,0 +1,56 @@
+package io.github.xinshepherd;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * 766. 托普利茨矩阵
+ *
+ * https://leetcode-cn.com/problems/toeplitz-matrix/
+ *
+ * @author Fuxin
+ * @since 2021/2/22
+ */
+public class ToeplitzMatrix {
+
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i = 0; i < n - 1; i++) {
+            if (isInvalid(matrix, 0, i))
+                return false;
+        }
+        for (int i = 1; i < m - 1; i++) {
+            if (isInvalid(matrix, i, 0))
+                return false;
+        }
+        return true;
+    }
+
+    private boolean isInvalid(int[][] matrix, int x, int y) {
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int target = matrix[x][y];
+        while (x < m && y < n) {
+            if (matrix[x][y] != target)
+                return true;
+            x++;
+            y++;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ToeplitzMatrix toeplitzMatrix = new ToeplitzMatrix();
+        assertThat(toeplitzMatrix.isToeplitzMatrix(new int[][]{
+                new int[]{1,2,3,4},
+                new int[]{5,1,2,3},
+                new int[]{9,5,1,2}
+        })).isTrue();
+        assertThat(toeplitzMatrix.isToeplitzMatrix(new int[][]{
+                new int[]{1,2},
+                new int[]{2,2}
+        })).isFalse();
+
+    }
+
+}
