02be0b51923a203ae6ca8c691ee441d5ba1300d3
XinShepherd <okfuxin@gmail.com>
2021-04-11 09:11:35 +0800

    二分查找优化

--- src/main/java/io/github/xinshepherd/BinarySearch.java
+++ src/main/java/io/github/xinshepherd/BinarySearch.java
@@ -28,6 +28,21 @@ public class BinarySearch {
         return -1;
     }
 
+    public int search2(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1); // 防止直接相加溢出
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid; // 因为只会有一次判断是否相等，大部分情况是不相等，放在最后
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         BinarySearch binarySearch = new BinarySearch();
         int[] nums = {-1, 0, 3, 5, 9, 12};
@@ -35,6 +50,8 @@ public class BinarySearch {
         assertThat(binarySearch.search(nums, 5)).isEqualTo(3);
         assertThat(binarySearch.search(nums, 6)).isEqualTo(-1);
         assertThat(binarySearch.search(nums, -1)).isEqualTo(0);
-        List<String> list = new ArrayList<>();
+        assertThat(binarySearch.search2(nums, 5)).isEqualTo(3);
+        assertThat(binarySearch.search2(nums, 6)).isEqualTo(-1);
+        assertThat(binarySearch.search2(nums, -1)).isEqualTo(0);
     }
 }
