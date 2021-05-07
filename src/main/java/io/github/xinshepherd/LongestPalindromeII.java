906b991bac828f01ae231fd48f6248642921268c
Fuxin <okfuxin@gmail.com>
2021-05-07 20:28:44 +0800

    5. 最长回文子串

--- src/main/java/io/github/xinshepherd/LongestPalindromeII.java
+++ src/main/java/io/github/xinshepherd/LongestPalindromeII.java
@@ -3,41 +3,31 @@ package io.github.xinshepherd;
 import org.assertj.core.api.Assertions;
 
 /**
- * 5. 最长回文子串
- *
- * https://leetcode-cn.com/problems/longest-palindromic-substring/
- *
  * @author Fuxin
  * @since 2021/5/7
  */
 public class LongestPalindromeII {
 
-    int left = -1;
-    int right = -1;
-    int max = 0;
     public String longestPalindrome(String s) {
         int len = s.length();
-        left = -1;
-        right = -1;
-        max = 0;
+        String result = "";
         for (int i = 0; i < len; i++) {
-            findStr(s, i, i);
-            findStr(s, i - 1, i);
+            result = findStr(s, i, i, result);
+            result = findStr(s, i - 1, i, result);
         }
-        return left == -1 ? "" : s.substring(left, right);
+        return result;
     }
 
-    private void findStr(String s, int left, int right) {
+    private String findStr(String s, int left, int right, String result) {
         int len = s.length();
         while (left >= 0 && right < len && s.charAt(left) == s.charAt(right)) {
             left--;
             right++;
         }
-        if (right - left - 1 > max) {
-            this.left = left + 1;
-            this.right = right;
-            this.max = right - left - 1;
+        if (right - left - 1 > result.length()) {
+            return s.substring(left + 1, right);
         }
+        return result;
     }
 
     public static void main(String[] args) {
