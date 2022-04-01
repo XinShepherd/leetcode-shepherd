d1a1d8d2c5faa2c6d4e1d3dd75579d374de34619
Fuxin <fuxin@itiger.com>
2022-04-01 16:59:15 +0800

    12 整数转罗马数字

--- src/main/java/io/github/xinshepherd/IntToRoman.java
+++ src/main/java/io/github/xinshepherd/IntToRoman.java
@@ -1,65 +1,39 @@
 package io.github.xinshepherd;
 
-//12 整数转罗马数字
+import org.assertj.core.api.Assertions;
+
+/**
+ * 12 整数转罗马数字
+ *
+ * https://leetcode-cn.com/problems/integer-to-roman/
+ */
 public class IntToRoman {
 
-    public String intToRoman(int num) {
-        StringBuilder stringBuilder = new StringBuilder();
-        intToRoman(num, stringBuilder);
-        return stringBuilder.toString();
-    }
+    int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    //太复杂了
-    private void intToRoman(int num, StringBuilder sb) {
-        if (num == 0) return;
-        if (num >= 1000) {
-            sb.append('M');
-            intToRoman(num - 1000, sb);
-        } else if (num >= 900) {
-            sb.append("CM");
-            intToRoman(num - 900, sb);
-        } else if (num >= 500) {
-            sb.append('D');
-            intToRoman(num - 500, sb);
-        } else if (num >= 400) {
-            sb.append("CD");
-            intToRoman(num - 400, sb);
-        } else if (num >= 100) {
-            sb.append('C');
-            intToRoman(num - 100, sb);
-        } else if (num >= 90) {
-            sb.append("XC");
-            intToRoman(num - 90, sb);
-        } else if (num >= 50) {
-            sb.append("L");
-            intToRoman(num - 50, sb);
-        } else if (num >= 40) {
-            sb.append("XL");
-            intToRoman(num - 40, sb);
-        } else if (num >= 10) {
-            sb.append("X");
-            intToRoman(num - 10, sb);
-        } else if (num >= 9) {
-            sb.append("IX");
-            intToRoman(num - 9, sb);
-        } else if (num >= 5) {
-            sb.append("V");
-            intToRoman(num - 5, sb);
-        } else if (num >= 4) {
-            sb.append("IV");
-            intToRoman(num - 4, sb);
-        } else if (num > 0) {
-            sb.append("I");
-            intToRoman(num - 1, sb);
+    public String intToRoman(int num) {
+        StringBuffer roman = new StringBuffer();
+        for (int i = 0; i < values.length; ++i) {
+            int value = values[i];
+            String symbol = symbols[i];
+            while (num >= value) {
+                num -= value;
+                roman.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
         }
+        return roman.toString();
     }
 
     public static void main(String[] args) {
         IntToRoman intToRoman = new IntToRoman();
-        System.out.println(intToRoman.intToRoman(3));
-        System.out.println(intToRoman.intToRoman(4));
-        System.out.println(intToRoman.intToRoman(9));
-        System.out.println(intToRoman.intToRoman(58));
-        System.out.println(intToRoman.intToRoman(1994));
+        Assertions.assertThat(intToRoman.intToRoman(3)).isEqualTo("III");
+        Assertions.assertThat(intToRoman.intToRoman(4)).isEqualTo("IV");
+        Assertions.assertThat(intToRoman.intToRoman(9)).isEqualTo("IX");
+        Assertions.assertThat(intToRoman.intToRoman(58)).isEqualTo("LVIII");
+        Assertions.assertThat(intToRoman.intToRoman(1994)).isEqualTo("MCMXCIV");
     }
 }
