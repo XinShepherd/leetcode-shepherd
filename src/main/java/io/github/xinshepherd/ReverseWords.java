1893f45b749d1b80875b6ccecb68cb213f7f2336
Fuxin <fuxin@itiger.com>
2023-02-16 17:00:42 +0800

    151. 翻转字符串里的单词

--- src/main/java/io/github/xinshepherd/ReverseWords.java
+++ src/main/java/io/github/xinshepherd/ReverseWords.java
@@ -1,5 +1,7 @@
 package io.github.xinshepherd;
 
+import org.assertj.core.api.Assertions;
+
 /**
  * 151. 翻转字符串里的单词
  *
@@ -23,9 +25,9 @@ public class ReverseWords {
 
     public static void main(String[] args) {
         ReverseWords reverseWords = new ReverseWords();
-        System.out.println(reverseWords.reverseWords("the sky is blue"));
-        System.out.println(reverseWords.reverseWords("  hello world!  "));
-        System.out.println(reverseWords.reverseWords("a good   example"));
+        Assertions.assertThat(reverseWords.reverseWords("the sky is blue")).isEqualTo("blue is sky the");
+        Assertions.assertThat(reverseWords.reverseWords("  hello world  ")).isEqualTo("world hello");
+        Assertions.assertThat(reverseWords.reverseWords("a good   example")).isEqualTo("example good a");
     }
 
 }
